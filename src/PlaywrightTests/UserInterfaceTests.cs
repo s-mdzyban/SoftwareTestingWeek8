125d9f886c3c7f26c94ee9f8fb2e2fa673f83dfa
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-28 22:49:16 -0800

    Update UserInterfaceTests.cs
    Made addition test

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -13,31 +13,8 @@ public class UserInterfaceTests
     [Fact]
     public static async Task VerifyGoogleSearchForPlaywright()
     {
-        using IPlaywright playwright = await Playwright.CreateAsync();
-        await using var browser =
-            await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions() { Headless = false, SlowMo = 50 });
+        //Verify you can add
+        Assert.Equal(4, 2+2);
 
-        IBrowserContext context = await browser.NewContextAsync();
-
-        IPage page = await context.NewPageAsync();
-        //Navigate to Google.com
-        await page.GotoAsync("https://www.google.com");
-        IReadOnlyList<IFrame> f = page.Frames;
-        if (f.Count > 1)
-        {
-            await f[1].ClickAsync("text=No thanks");
-        }
-        // Search Playwright
-        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
-        // Press Enter
-        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
-        //Click on the first search option
-        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
-        //Verify Page URL
-        Assert.Equal("https://playwright.dev/", page.Url);
-        // Click text=Get started
-        await page.ClickAsync("text=Get Started");
-        //Verify Page URL
-        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
     }
 }
