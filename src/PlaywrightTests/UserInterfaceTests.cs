a2f080619bb41cbcfb1671ec1f8842a6f6ea9a06
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-29 20:07:25 -0800

    Update UserInterfaceTests.cs
    hybrid between 2+2 and Selenium log in test

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -1,37 +1,26 @@
-using System;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+// Copyright Information
+// ==================================
+// SoftwareTesting - PlaywrightTests - UserInterfaceTests.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2022/07/22
+// ==================================
 
-public class MortgageTest
+namespace PlaywrightTests;
+
+public class UserInterfaceTests
 {
-    private static IWebDriver driver = new ChromeDriver();
-    
-    public string RunTest()
+    //https://medium.com/version-1/playwright-a-modern-end-to-end-testing-for-web-app-with-c-language-support-c55e931273ee#:~
+    [Fact]
+    public static async Task VerifyGoogleSearchForPlaywright()
     {
-		Test();
-		return ChromeDriver.GetLog();
-    }
-
-    public void Test()
-    {
-        //Insert your code only here
-            driver.Navigate().GoToUrl("https://www.moneyunder30.com/mortgage-calculator/");
-            driver.FindElement(By.Id("fieldname23_1")).Click();
-            driver.FindElement(By.Id("fieldname23_1")).Clear();
-            driver.FindElement(By.Id("fieldname23_1")).SendKeys("$100,000.00");
-            driver.FindElement(By.Id("fieldname24_1")).Click();
-            driver.FindElement(By.Id("fieldname24_1")).Click();
-            driver.FindElement(By.Id("fieldname24_1")).Clear();
-            driver.FindElement(By.Id("fieldname24_1")).SendKeys("$0.00");
-            driver.FindElement(By.Id("fieldname25_1")).Click();
-            driver.FindElement(By.Id("fieldname25_1")).Clear();
-            driver.FindElement(By.Id("fieldname25_1")).SendKeys("3.50%");
-            driver.FindElement(By.Id("fieldname19_1")).Click();
-            driver.FindElement(By.Id("fieldname19_1")).Click();
-            driver.FindElement(By.Id("fieldname19_1")).Clear();
-            driver.FindElement(By.Id("fieldname19_1")).SendKeys("30");
-            driver.FindElement(By.Id("fieldname26_1")).Click();
-            Assert.AreEqual("$449.04", driver.FindElement(By.Id("fieldname20_1")).GetAttribute("value"));
+            driver.Navigate().GoToUrl("https://www.letsusedata.com/");
+            driver.FindElement(By.Id("txtUser")).Click();
+            driver.FindElement(By.Id("txtUser")).Clear();
+            driver.FindElement(By.Id("txtUser")).SendKeys("Test2");
+            driver.FindElement(By.Id("txtPassword")).Click();
+            driver.FindElement(By.Id("txtPassword")).Clear();
+            driver.FindElement(By.Id("txtPassword")).SendKeys("iF3sBF7c");
+            driver.FindElement(By.Id("javascriptLogin")).Click();
 
     }
 }
