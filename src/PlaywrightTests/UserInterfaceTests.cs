07081c4451d53435f5a308c04e6bdc8bbef78f41
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-30 12:01:13 -0800

    Update UserInterfaceTests.cs
    Line 44: adding URL

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -40,6 +40,7 @@ public class UserInterfaceTests
         //Assert.Equal("https://www.letsusedata.com/CourseSelection.html", page.Url);
         //Assert.Equal(page.GetByText("Invalid Password"));
         //await expect(page.GetByText("Invalid Password")).ToBeVisibleAsync();
-        await page.locator(page.GetByText("Invalid Password"));
+        //await page.locator(page.GetByText("Invalid Password"));
+        await page.locator(page.Url, { hasText: "Invalid Password" })
     }
 }
