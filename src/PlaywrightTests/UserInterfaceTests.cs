d325253545b68f621ac80a0310d818cd289653c8
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-30 10:45:58 -0800

    Update UserInterfaceTests.cs
    Line 28: change aria element to #text

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -25,7 +25,8 @@ public class UserInterfaceTests
 
         //await page.ClickAsync("#txtUser");
         //await page.type("text=Your Username", "Test2");
-        await page.FillAsync("[aria-label=\"Your Username\"]", "Test2");
+        
+        await page.FillAsync("#txtUser", "Test2");
         await page.FillAsync("[aria-label=\"Your Password\"]", "iF3sBF7c");                             
         // Press Enter
         var response = await page.RunAndWaitForNavigationAsync(async () => await page.ClickAsync("[aria-label=\"Login\"]"));
