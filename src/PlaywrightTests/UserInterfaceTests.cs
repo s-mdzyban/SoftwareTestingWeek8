375635a6cf0831a2f8a5d372832124286d947209
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-30 00:20:11 -0800

    Update UserInterfaceTests.cs
    Attempt at running Test2 for Letsusedata

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -21,23 +21,16 @@ public class UserInterfaceTests
 
         IPage page = await context.NewPageAsync();
         //Navigate to Google.com
-        await page.GotoAsync("https://www.google.com");
-        IReadOnlyList<IFrame> f = page.Frames;
-        //if (f.Count > 1)
-        //{
-       //     await f[1].ClickAsync("text=No thanks");
-        //}
-        // Search Playwright
-        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
+        await page.GotoAsync("https://www.letsusedata.com/index.html");
+
+        await page.FillAsync("[aria-label=\"Your Username"]", "Test2");
+        await page.FillAsync("[aria-label=\"Your Password"]", "iF3sBF7c");                             
         // Press Enter
-        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
+        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Login"]", "Enter"));
         //Click on the first search option
-        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
+        await page.ClickAsync("xpath=//h3[contains(text(),'Intro to Data Analytics')]");
         //Verify Page URL
-        Assert.Equal("https://playwright.dev/", page.Url);
-        // Click text=Get started
-        await page.ClickAsync("text=Get Started");
-        //Verify Page URL
-        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
+        Assert.Equal("https://www.letsusedata.com/CourseSelection.html", page.Url);
+        
     }
 }
