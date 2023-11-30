99c014bf750e384460f5f614c9eee693992e7e56
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-29 19:59:55 -0800

    Update UserInterfaceTests.cs
    Inserted Mortgage test into YAML

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -1,20 +1,37 @@
-// Copyright Information
-// ==================================
-// SoftwareTesting - PlaywrightTests - UserInterfaceTests.cs
-// All samples copyright Philip Japikse
-// http://www.skimedic.com 2022/07/22
-// ==================================
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace PlaywrightTests;
-
-public class UserInterfaceTests
+public class MortgageTest
 {
-    //https://medium.com/version-1/playwright-a-modern-end-to-end-testing-for-web-app-with-c-language-support-c55e931273ee#:~
-    [Fact]
-    public static async Task VerifyGoogleSearchForPlaywright()
+    private static IWebDriver driver = new ChromeDriver();
+    
+    public string RunTest()
     {
-        //Verify you can add
-        Assert.Equal(4, 2+2);
+		Test();
+		return ChromeDriver.GetLog();
+    }
+
+    public void Test()
+    {
+        //Insert your code only here
+            driver.Navigate().GoToUrl("https://www.moneyunder30.com/mortgage-calculator/");
+            driver.FindElement(By.Id("fieldname23_1")).Click();
+            driver.FindElement(By.Id("fieldname23_1")).Clear();
+            driver.FindElement(By.Id("fieldname23_1")).SendKeys("$100,000.00");
+            driver.FindElement(By.Id("fieldname24_1")).Click();
+            driver.FindElement(By.Id("fieldname24_1")).Click();
+            driver.FindElement(By.Id("fieldname24_1")).Clear();
+            driver.FindElement(By.Id("fieldname24_1")).SendKeys("$0.00");
+            driver.FindElement(By.Id("fieldname25_1")).Click();
+            driver.FindElement(By.Id("fieldname25_1")).Clear();
+            driver.FindElement(By.Id("fieldname25_1")).SendKeys("3.50%");
+            driver.FindElement(By.Id("fieldname19_1")).Click();
+            driver.FindElement(By.Id("fieldname19_1")).Click();
+            driver.FindElement(By.Id("fieldname19_1")).Clear();
+            driver.FindElement(By.Id("fieldname19_1")).SendKeys("30");
+            driver.FindElement(By.Id("fieldname26_1")).Click();
+            Assert.AreEqual("$449.04", driver.FindElement(By.Id("fieldname20_1")).GetAttribute("value"));
 
     }
 }
