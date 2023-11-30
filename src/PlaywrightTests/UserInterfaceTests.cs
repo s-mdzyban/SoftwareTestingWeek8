a0820d995e6894e014f2cd09f8033854a2568b62
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-29 22:33:24 -0800

    Update UserInterfaceTests.cs
    Restoring Original Import CS file

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -1,104 +1,43 @@
-using System;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
+// Copyright Information
+// ==================================
+// SoftwareTesting - PlaywrightTests - UserInterfaceTests.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2022/07/22
+// ==================================
 
-namespace SeleniumTests
+namespace PlaywrightTests;
+
+public class UserInterfaceTests
 {
-    [TestClass]
-    public class UntitledTestCase
+    //https://medium.com/version-1/playwright-a-modern-end-to-end-testing-for-web-app-with-c-language-support-c55e931273ee#:~
+    [Fact]
+    public static async Task VerifyGoogleSearchForPlaywright()
     {
-        private static IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private static string baseURL;
-        private bool acceptNextAlert = true;
-        
-        [ClassInitialize]
-        public static void InitializeClass(TestContext testContext)
+        using IPlaywright playwright = await Playwright.CreateAsync();
+        await using var browser =
+            await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions() { Headless = false, SlowMo = 50 });
+
+        IBrowserContext context = await browser.NewContextAsync();
+
+        IPage page = await context.NewPageAsync();
+        //Navigate to Google.com
+        await page.GotoAsync("https://www.google.com");
+        IReadOnlyList<IFrame> f = page.Frames;
+        if (f.Count > 1)
         {
-            driver = new FirefoxDriver();
-            baseURL = "https://www.google.com/";
+            await f[1].ClickAsync("text=No thanks");
         }
-        
-        [ClassCleanup]
-        public static void CleanupClass()
-        {
-            try
-            {
-                //driver.Quit();// quit does not close the window
-                driver.Close();
-                driver.Dispose();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-        }
-        
-        [TestInitialize]
-        public void InitializeTest()
-        {
-            verificationErrors = new StringBuilder();
-        }
-        
-        [TestCleanup]
-        public void CleanupTest()
-        {
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
-        
-        [TestMethod]
-        public void TheUntitledTestCaseTest()
-        {
-            driver.Navigate().GoToUrl("https://www.letsusedata.com/");
-            driver.FindElement(By.Id("txtUser")).Click();
-            driver.FindElement(By.Id("txtPassword")).Clear();
-            driver.FindElement(By.Id("txtPassword")).SendKeys("iF3sBF7c");
-            driver.FindElement(By.Id("javascriptLogin")).Click();
-        }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-        
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-        
-        private string CloseAlertAndGetItsText() {
-            try {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert) {
-                    alert.Accept();
-                } else {
-                    alert.Dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
-        }
+        // Search Playwright
+        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
+        // Press Enter
+        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
+        //Click on the first search option
+        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
+        //Verify Page URL
+        Assert.Equal("https://playwright.dev/", page.Url);
+        // Click text=Get started
+        await page.ClickAsync("text=Get Started");
+        //Verify Page URL
+        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
     }
 }
