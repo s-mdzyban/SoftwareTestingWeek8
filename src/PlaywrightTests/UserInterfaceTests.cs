159b9f60d7e149d1f3115d2d24008de98d79294b
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-29 20:15:13 -0800

    Update UserInterfaceTests.cs
    Restore complete Selenium Log In Export

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -1,19 +1,47 @@
 using System;
 using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
-public class MortgageTest
+namespace SeleniumTests
 {
-    private static IWebDriver driver = new ChromeDriver();
-    
-    public string RunTest()
+    [TestFixture]
+    public class LetsUseDataLogin
     {
-		Test();
-		return ChromeDriver.GetLog();
-    }
-
-    public void Test()
-    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheLetsUseDataLoginTest()
+        {
             driver.Navigate().GoToUrl("https://www.letsusedata.com/");
             driver.FindElement(By.Id("txtUser")).Click();
             driver.FindElement(By.Id("txtUser")).Clear();
@@ -22,6 +50,46 @@ public class MortgageTest
             driver.FindElement(By.Id("txtPassword")).Clear();
             driver.FindElement(By.Id("txtPassword")).SendKeys("iF3sBF7c");
             driver.FindElement(By.Id("javascriptLogin")).Click();
-
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
     }
 }
