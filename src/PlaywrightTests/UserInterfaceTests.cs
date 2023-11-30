256af53139cce94282c7b0bd04e634dc2b9d139f
s-mdzyban <152228076+s-mdzyban@users.noreply.github.com>
2023-11-30 12:54:58 -0800

    Update UserInterfaceTests.cs

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -42,7 +42,7 @@ public class UserInterfaceTests
         //await expect(page.GetByText("Invalid Password")).ToBeVisibleAsync();
         //await page.locator(page.GetByText("Invalid Password"));
         //await Expect(page.GetByText("Invalid Password")).ToBeVisibleAsync();
-        var test1Expected = string("Invalid Password");
+        var test1Expected = "Invalid Password";
         Assert.Equal(test1Expected, page.GetByText("Invalid Password"));
         
         //await page('span')
